88249c0c784772bd0aed3fc289b670c943384b34
nevezhinda <NevezhinDA@admlr.lipetsk.ru>
2025-06-03 19:48:13 +0300

    feat (exception): add handler for json and a file process

--- src/main/java/com/org/test/validatenametest/exception/ControllerAdviceExceptionHandler.java
+++ src/main/java/com/org/test/validatenametest/exception/ControllerAdviceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.org.test.validatenametest.exception;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,14 +8,23 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.io.IOException;
+
 @Hidden
 @RestControllerAdvice
 public class ControllerAdviceExceptionHandler {
 
     @ExceptionHandler({NotFoundRequestContentException.class,
             NotFoundSettingException.class})
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     ResponseEntity<String> handleValidationException(RuntimeException  exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler({JsonProcessingException.class,
+            IOException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    ResponseEntity<String> handleProcessException(RuntimeException  exception) {
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }
