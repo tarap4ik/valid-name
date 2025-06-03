c52ea06457cb5539b12c5387f71e51425ba48584
nevezhinda <NevezhinDA@admlr.lipetsk.ru>
2025-06-03 23:44:34 +0300

    feat (dto): add validation at settings dto

--- src/main/java/com/org/test/validatenametest/dto/SettingsDTO.java
+++ src/main/java/com/org/test/validatenametest/dto/SettingsDTO.java
@@ -1,6 +1,8 @@
 package com.org.test.validatenametest.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 import java.io.Serializable;
 
-public record SettingsDTO(String name, String value) implements Serializable {
+public record SettingsDTO(@NotBlank String name, @NotBlank String value) implements Serializable {
 }
