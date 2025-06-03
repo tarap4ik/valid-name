78c25d110261c0a8370718779fb77f738c435052
nevezhinda <NevezhinDA@admlr.lipetsk.ru>
2025-06-03 23:48:16 +0300

    feat (handler): add handlers for validation

--- src/main/java/com/org/test/validatenametest/exception/ControllerAdviceExceptionHandler.java
+++ src/main/java/com/org/test/validatenametest/exception/ControllerAdviceExceptionHandler.java
@@ -2,13 +2,17 @@ package com.org.test.validatenametest.exception;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import io.swagger.v3.oas.annotations.Hidden;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.io.IOException;
+import java.util.List;
 
 @Hidden
 @RestControllerAdvice
@@ -27,4 +31,21 @@ public class ControllerAdviceExceptionHandler {
     ResponseEntity<String> handleProcessException(RuntimeException  exception) {
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler({ConstraintViolationException .class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    ResponseEntity<String> handleConstraintValidateException(RuntimeException  exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException .class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    ResponseEntity<List<String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException  exception) {
+        List<String> errors = exception.getBindingResult().getAllErrors().stream()
+                .map(err -> err.unwrap(ConstraintViolation.class))
+                .map(err -> String.format("'%s' %s", err.getPropertyPath(), err.getMessage()))
+                .toList();
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
 }
